e2bf223df207cf6d15e8f150f586f6dbb789cdab
Denis Osipychev <denisosipychev@wirelessprv-10-195-105-26.near.illinois.edu>
2017-06-20 17:53:25 -0500

    mdp research added

---
+++ research/main.js
@@ -0,0 +1,182 @@
+//------------------------------------------------------
+	
+//-- CONSTANTS & GLOBAL VARIABLES
+var n_iter = 0;
+var myGrid;
+var canvas;
+var n_dim;
+var state = [0,0];
+var n_actions = 5;
+var sim_states = {reset:0,ready:4,run:5,manual:6};
+var sim_state = sim_states.reset;
+var mouse_pos = [];
+var reward_hist = [];
+
+//-- Experiment globals
+var n_episode = 0;
+var cum_reward = 0;
+var hist = ['History\n'];
+
+//MAIN
+function main() {
+    canvas = document.getElementById('example');
+    if (! canvas) {
+        console.log(' Failed to retrieve the < canvas > element');
+        return false;
+    }
+    else {
+	    console.log(' Got < canvas > element ');
+    }
+    
+    canvas.addEventListener("mousedown", function(evt){
+        var rect = canvas.getBoundingClientRect();
+        mouse_pos = [evt.clientX - rect.left, evt.clientY - rect.top];
+        selection(mouse_pos);
+        console.log("Mouse click at:", mouse_pos);
+    }, false);
+    
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+    
+    generate();
+    animate();
+}
+
+function animate(){
+	setInterval(update, 10);
+}
+
+function save_to_file(){
+    var hiddenElement = document.createElement('a');
+
+    hiddenElement.href = 'data:attachment/text,' + encodeURI(hist);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'data.txt';
+    hiddenElement.click();
+}
+
+//CONTROL
+function sim_run(){
+    sim_state = sim_states.run;
+}
+
+function sim_stop(){
+    sim_state = sim_states.ready;
+}
+
+//SIMULATION
+function generate(){
+    n_dim = document.getElementById("n_dim").value;
+    myGrid = new UGrid2D([-1.,-1.],[1.,1.],n_dim);
+    mdp_init(n_dim);
+}
+
+function singleUpdate(){
+    sim_state = sim_states.run;
+    update();
+    sim_state = sim_states.ready;
+}
+
+function update(){
+    
+    if (sim_state == sim_states.reset){
+        n_iter = 0;
+        n_episode += 1;
+        cum_reward = 0;
+        state = mdp_random_state(n_dim);
+        
+        sim_state = sim_states.ready;
+    }
+    
+    var epsilon = document.getElementById("epsilon").value;
+    var max_iter = document.getElementById("num_iter").value
+    
+    // DRAW AND UPDATE
+    draw();
+    
+	if (n_iter < max_iter && (sim_state == sim_states.run) ){
+		
+        var action=-1;
+        
+        if(document.getElementById("ctrl_RL").checked){
+            if (Math.random() < epsilon){
+                action = findMaxInd(get_qvalue(state));
+                console.log("best action");
+            }
+            else{
+                action = Math.floor(Math.random() * 5);
+                console.log("random action");
+            }
+        }
+        else if(document.getElementById("ctrl_human").checked){
+            handleKeys();
+            if (keyEvents.active.left){
+                action = 1;
+                keyEvents.active.left = false;
+            }
+            else if (keyEvents.active.right){
+                action = 2;
+                keyEvents.active.right = false;
+            }  
+            else if (keyEvents.active.up){
+                action = 3;
+                keyEvents.active.up = false;
+            }
+            else if (keyEvents.active.down){
+                action = 4;
+                keyEvents.active.down = false;
+            } 
+        }
+
+        if (action > -1){
+            n_iter += 1;
+            console.log("Action taken:",action);
+            var prev_state = state.slice();
+            state = mdp_step(state,action,n_dim);
+            var rwrd = reward[state[0]][state[1]];
+            update_qvalue(prev_state,state,action,rwrd);
+        
+        cum_reward += rwrd;
+        if (term_map[state[0]][state[1]] == true){
+            sim_state = sim_states.reset;
+            hist += 'Num iter: ' + n_iter + ' Cum reward: ' + cum_reward + '\n';
+            reward_hist.push({x:n_episode,y:cum_reward});
+            console.log(reward_hist);
+            plot_result(reward_hist);
+        }
+        }
+	}
+}
+
+function plot_result(reward_hist){
+    Highcharts.chart('container', {
+                     chart: {type: 'scatter',zoomType: 'xy'},
+                     title: {text: 'Cumulative Reward Vs. Episode'},
+                     xAxis: {title: {enabled: true,text: 'Episode'},
+                        startOnTick: true,endOnTick: true,showLastLabel: true},
+                     yAxis: {title: {text: 'Cumulative Reward'}},
+                     legend: {layout: 'vertical',align: 'left',verticalAlign: 'top',x: 100,y: 70,floating: true,
+                        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',borderWidth: 1},
+                     plotOptions: {scatter: {marker: {radius: 5,states: {hover: {enabled: true,lineColor: 'rgb(100,100,100)'}}},
+                     states: {hover: {marker: {enabled: false}}},
+                     tooltip: {headerFormat: '<b>{series.name}</b><br>',pointFormat: 'Episode: {point.x}, Reward: {point.y}'}}},
+                     series: [{lineWidth:2,name: 'Reward',color: 'rgba(223, 83, 83, .5)',data: reward_hist}]});
+}
+
+function draw(){
+    // Get the rendering context for 2DCG <- (2)
+    var ctx = canvas.getContext('2d');
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (document.getElementById("show_goal").checked) myGrid.show_colors(canvas,reward,n_dim);
+    
+    myGrid.show_state(canvas,state,n_dim);
+    myGrid.draw_grid(canvas);
+    
+    if (document.getElementById("show_R").checked)
+        myGrid.show_values(canvas,reward,n_dim);
+    else if (document.getElementById("show_Q").checked)
+        myGrid.show_qvalues(canvas,qvalue,n_dim,n_actions);
+    else if (document.getElementById("show_Pi").checked)
+        myGrid.show_policy(canvas,qvalue,n_dim,n_actions);
+}
---
+++ research/mdp.js
@@ -0,0 +1,165 @@
+var reward     = [];
+var policy     = [];
+var value      = [];
+var qvalue     = [];
+var transition = [];
+var term_map = [];
+
+function mdp_init(n_states){
+    mdp_init_transition(n_states);
+    for (var i=0; i < n_states; ++i){
+        reward[i] = new Array(n_states);
+        policy[i] = new Array(n_states);
+        value[i] = new Array(n_states);
+        qvalue[i] = new Array(n_states);
+        term_map[i] = new Array(n_states);
+        for (var j=0; j < n_states; ++j){
+            reward[i][j] = -1.0;
+            policy[i][j] = 0;
+            value[i][j] = 0.0;
+            term_map[i][j] = false;
+            qvalue[i][j] = new Array(n_actions);
+            for (var k=0; k < n_actions; ++k){
+                qvalue[i][j][k] = 0.0;
+            }
+        }
+    }
+    mdp_init_reward(n_states);
+}
+
+function mdp_step(current_state,action,n_states){
+    
+    if (document.getElementById("norm_trans").checked){
+        if (action == 0){
+            current_state = current_state;
+        }
+        if (action == 1){
+            current_state[0] = Math.max(0,current_state[0]-1);
+        }
+        if (action == 2){
+            current_state[0] = Math.min(n_dim-1,current_state[0]+1);
+        }
+        if (action == 3){
+            current_state[1] = Math.max(0,current_state[1]-1);
+        }
+        if (action == 4){
+            current_state[1] = Math.min(n_dim-1,current_state[1]+1);
+        }
+    }
+    if (document.getElementById("rand_trans").checked){
+        current_state_1D = current_state[0] + current_state[1]*(n_states);
+        new_state = get_transition(current_state_1D, action, n_states);
+    
+        current_state[0] = Math.floor(new_state % (n_states));
+        current_state[1] = Math.floor(new_state / (n_states));
+    
+        console.log("transition state for action:", current_state[0], current_state[1]);
+    }
+
+    
+    return current_state;
+}
+
+function update_qvalue(current_state, new_state, action, rew){
+    
+    var alpha = document.getElementById("alpha").value;
+    var gamma = document.getElementById("gamma").value;
+    
+    var qnext = get_qvalue(new_state);
+    var maxQnext = findMaxVal(qnext);
+    var learned = rew + gamma*maxQnext - get_qvalue(current_state)[action];
+    
+    qvalue[current_state[0]][current_state[1]][action] += alpha * learned;
+}
+                                                   
+function get_qvalue(current_state){
+    return qvalue[current_state[0]][current_state[1]];
+}
+
+function get_transition(current_state_1D, action, n_states){
+    new_state = 0;
+    for (var k=0; k < n_states*n_states; ++k){
+        if (transition[action][current_state_1D][k] == 1.0){
+            new_state = k;
+            break;
+        }
+        else{
+            new_state = current_state_1D;
+        }
+    }
+    return new_state;
+}
+
+function mdp_init_transition(n_states){
+    for (var i=0; i < n_actions; ++i){
+        transition[i] = new Array(n_actions);
+        for (var j=0; j < n_states*n_states; ++j){
+            transition[i][j] = new Array(n_states*n_states);
+            for (var k=0; k < n_states*n_states; ++k){
+                transition[i][j][k] = 0.0;
+            }
+        }
+    }
+    
+    for (var i=0; i < n_actions; ++i){
+        for (var j=0; j < n_states*n_states; ++j){
+            rand = Math.floor((Math.random() * (n_states*n_states - 1)) + 0);
+            transition[i][j][rand] = 1.0;
+        }
+    }
+}
+
+function mdp_init_reward(n_states){
+    
+    [xi,yi] = mdp_random_state(n_states);
+    reward[xi][yi] += 100;
+    term_map[xi][yi] = true;
+    
+    [xi,yi] = mdp_random_state(n_states);
+    reward[xi][yi] -= 100;
+    term_map[xi][yi] = true;
+}
+
+function mdp_random_state(n_states){
+    var all_clr = false;
+    
+    while (!all_clr){
+        var rand1 = Math.floor((Math.random() * (n_states - 1)) + 0);
+        var rand2 = Math.floor((Math.random() * (n_states - 1)) + 0);
+        if (term_map[rand1][rand2] == false ) all_clr = true;
+        s = [rand1, rand2];
+    }
+    return s;
+}
+
+
+//SERVICE FUNCTIONS
+function findMaxInd(arr) {
+    if (arr.length == 0)
+        return -1;
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++)
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+
+    return maxIndex;
+}
+
+function findMaxVal(arr) {
+    if (arr.length == 0)
+        return -1;
+    
+    var max = arr[0];
+    
+    for (var i = 1; i < arr.length; i++)
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    
+    return max;
+}
